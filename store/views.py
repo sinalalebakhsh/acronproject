b750a07693250a2312b90bfbd8a7183e92332e0c
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-10-04 23:45:11 +0330

    11

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello Sina')
+    return render(request, 'hello.html' )
 
 
 def numbers_(request, numbers):
