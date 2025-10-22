d85ffb67870248fc7c1bf380896e523d3c57b48a
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-10-22 22:17:27 +0330

    14

--- store/models.py
+++ store/models.py
@@ -8,3 +8,17 @@ from django.db import models
 # description
 # price
 # add date and time
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+
+
+
