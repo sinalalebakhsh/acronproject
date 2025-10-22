44f955edd2fee6850b908bf0056851dff984b24a
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-10-22 22:21:25 +0330

    15

--- store/models.py
+++ store/models.py
@@ -12,6 +12,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
