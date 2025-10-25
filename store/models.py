9463bb4acd8593a0616483610a2f7fb8b861160a
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-10-25 17:21:14 +0330

    21

--- store/models.py
+++ store/models.py
@@ -33,6 +33,7 @@ class Customer(models.Model):
 
 
 class Order(models.Model):
+    # customer => Foreign Key
     ORDER_STATUS_PAID = 'P'
     ORDER_STATUS_UNPAID = 'U'
     ORDER_STATUS_CANCELED = 'C'
@@ -43,6 +44,27 @@ class Order(models.Model):
     ]
     datetime_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=255, choices=ORDER_STATUS, default='u')
+
+# Comment
+# name
+# body
+# date of creation
+# status { Waiting OR Aproved OR Not_Aproved}
+class Comment(models.Model):
+    # product => Foreign Key
+    COMMENT_STATUS_WAITING = 'w'
+    COMMENT_STATUS_APROVED = 'a'
+    COMMENT_STATUS_NOT_APROCED = 'n'
+    COMMENT_STATUS = [
+        (COMMENT_STATUS_WAITING,'waiting'),
+        (COMMENT_STATUS_APROVED,'aproved'),
+        (COMMENT_STATUS_NOT_APROCED,'not aproved'),
+    ]
+    name = ""
+    body = models.TextField(max_length=255)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, choices=COMMENT_STATUS, default='w')
+
 
 
 
