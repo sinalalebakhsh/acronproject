40b2dc07b78aaecc1eb2216815d3f5e3ec0f9c52
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-10-25 18:09:01 +0330

    23

--- store/models.py
+++ store/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500, blank=True)
+    detetime_created = models.DateTimeField(auto_now_add=True)
 
 # Product
 # name
@@ -10,13 +14,13 @@ from django.db import models
 # add date and time
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField(default=0)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-
 
 # Customer:
 #   first_name
@@ -34,6 +38,7 @@ class Customer(models.Model):
 
 class Order(models.Model):
     # customer => Foreign Key
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     ORDER_STATUS_PAID = 'P'
     ORDER_STATUS_UNPAID = 'U'
     ORDER_STATUS_CANCELED = 'C'
@@ -43,7 +48,7 @@ class Order(models.Model):
         (ORDER_STATUS_CANCELED ,'Canceled'),
     ]
     datetime_created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=255, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
 
 # Comment
 # name
@@ -52,6 +57,8 @@ class Order(models.Model):
 # status { Waiting OR Aproved OR Not_Aproved}
 class Comment(models.Model):
     # product => Foreign Key
+    # One Many
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     COMMENT_STATUS_WAITING = 'w'
     COMMENT_STATUS_APROVED = 'a'
     COMMENT_STATUS_NOT_APROCED = 'na'
