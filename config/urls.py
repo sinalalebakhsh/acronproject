33684d7693856585e20bba721964caef17eb35c1
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-11-01 13:39:59 +0330

    26-5

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    # path('__debug__', include('debug_toolbar.urls')),
 ] + debug_toolbar_urls()
 
 
