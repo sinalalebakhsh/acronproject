72cf7cd3fc2d6dc698c1d6fe7319b0571c1ca6c2
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-11-06 22:38:49 +0330

    26.10.5

--- store/views.py
+++ store/views.py
@@ -6,7 +6,9 @@ from .models import Product, OrderItem
 
 def show_data(request):
     queryset = Product.objects.all()
+    queryset = Product.objects.filter(inventory__lt=50)
     queryset = Product.objects.order_by('inventory')
+    
     # queryset = OrderItem.objects.all()
 
     list(queryset)
