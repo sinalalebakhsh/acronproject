26b84a57567a7afed013c03b0c20c7796760a00f
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-11-09 06:30:03 +0330

    26.10.8 Q object

--- store/views.py
+++ store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 
 from .models import Product, OrderItem, Order
 
 
 def show_data(request):
-    queryset = Order.objects.exclude(status=Order.ORDER_STATUS_UNPAID)
+    queryset = Product.objects.filter(Q(inventory__lt=5) | Q(inventory__gt=96)).order_by('inventory')
     
     list(queryset)
     return render(request, 'hello.html',)
