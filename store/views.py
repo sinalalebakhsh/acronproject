a8bec8b7dee964c7eb2208af9fc1f81d170d58e3
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-11-15 17:32:42 +0330

    26.10.20 only()

--- store/views.py
+++ store/views.py
@@ -5,10 +5,8 @@ from django.db.models import Q, F
 from .models import Product, OrderItem, Order
 
 
-def show_data(request):
-    queryset_orderitems_products = OrderItem.objects.values('product_id').distinct() # delete repetetive products if had to 
-    
-    queryset = Product.objects.filter(id__in=queryset_orderitems_products)
+def show_data(request):    
+    queryset = Product.objects.only('name', 'id')
 
     # list(queryset_orderitems_products)
     return render(request, 'hello.html', {'products': list(queryset)})
