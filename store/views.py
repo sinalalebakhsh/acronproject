e9adb28a576f289a72517373f16dfc8a23720076
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-11-17 12:38:55 +0330

    26.10.24 practice

--- store/views.py
+++ store/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
 
-from .models import Product, OrderItem, Order
+from .models import Product, OrderItem, Order, Comment
 
 
 def show_data(request):    
-    queryset = OrderItem.objects.select_related('order', 'product').all()
+    queryset = Comment.objects.prefetch_related('product').all()
 
     # list(queryset_orderitems_products)
-    return render(request, 'hello.html', {'ordered_items': list(queryset)})
+    return render(request, 'hello.html', {'comments': list(queryset)})
 
 
 
