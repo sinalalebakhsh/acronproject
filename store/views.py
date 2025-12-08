d44e46895d5ea9b2177d31a743cd86cf5869e950
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-09 02:21:22 +0330

    26.10.35 add Avg and Max

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from django.db.models import Count
+from django.db.models import Count, Avg, Max
 
 from .models import Product, OrderItem, Order, Comment
 
@@ -10,7 +10,11 @@ def show_data(request):
     queryset = Product.objects.all()
     # To PUSH
     # list(queryset_orderitems_products)
-    queryset_plus = Product.objects.aggregate(id=Count('id'))
+    queryset_plus = Product.objects.aggregate(
+        id=Count('id'),
+        avrage=Avg('unit_price'),
+        max_inventory=Max('inventory'),
+        )
     
     return render(request, 'hello.html', {
         'order_items': list(queryset),
