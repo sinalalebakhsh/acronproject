56e5510dd42d788ff9ffca1d2dbed3f150b88625
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-10 12:26:49 +0330

    26.10.40 use defer for unshow first name and last names[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[D[D[D[D[D[D[D[D[D[D[Cs

--- store/views.py
+++ store/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from django.db.models import Q, F, Value, Func
 from django.db.models import Count, Avg, Max
 
-from .models import Product, OrderItem, Order, Comment
+from .models import Product, OrderItem, Order, Comment, Customer
 
 
 def show_data(request):    
-    queryset = OrderItem.objects.annotate(fullname=Func(F('first_name'), Value(' '), F('last_name'),function='CONCAT'))
+    queryset = Customer.objects.annotate(fullname=Func(F('first_name'), Value(' '), F('last_name'),function='CONCAT')).defer('first_name', 'last_name')
     
     print(list(queryset))
     return render(request, 'hello.html', { })
