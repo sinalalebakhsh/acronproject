44b9efa4dfde022cf8e6dd4392f29ef8ee1693cf
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-10 12:23:28 +0330

    26.10.39 use Func ORM for database

--- store/views.py
+++ store/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F, Value
+from django.db.models import Q, F, Value, Func
 from django.db.models import Count, Avg, Max
 
 from .models import Product, OrderItem, Order, Comment
 
 
 def show_data(request):    
-    queryset = OrderItem.objects.annotate(total_price=F('quantity') * F('unit_price')).all()
+    queryset = OrderItem.objects.annotate(fullname=Func(F('first_name'), Value(' '), F('last_name'),function='CONCAT'))
     
     print(list(queryset))
     return render(request, 'hello.html', { })
