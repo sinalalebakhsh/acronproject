237b0c2769427e049dbd8b61dd244236d129cdb7
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-10 06:04:36 +0330

    26.10.37 annotate

--- store/views.py
+++ store/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Q, F
+from django.db.models import Q, F, Value
 from django.db.models import Count, Avg, Max
 
 from .models import Product, OrderItem, Order, Comment
 
 
 def show_data(request):    
-    queryset = Product.objects.all()
-    # To PUSH
-    # list(queryset_orderitems_products)
-    queryset_plus = Product.objects.filter(inventory__gt=10).aggregate(avregate_of_inventory_gt_10=Avg('unit_price'))
+    queryset = Product.objects.annotate(x=Value(1)).all()[:5]
     
+       
     return render(request, 'hello.html', {
-        'order_items': list(queryset),
-        'aggregate': queryset_plus,
+        
+
         })
 
 
