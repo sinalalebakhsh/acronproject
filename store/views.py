6459cb070fa22cc1431a55c13799faaf85f5f4df
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-14 18:29:26 +0330

    14.12.25 18:29:26 Django project changes:
    Modified files:
    - it_auto_log.txt
    - store/views.py

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F, Value, Func
 from django.db.models import Count, Avg, Max
+from django.db.models import ExpressionWrapper
 
 
 
@@ -9,9 +10,10 @@ from .models import Product, OrderItem, Order, Comment, Customer
 
 
 def show_data(request):    
-    queryset = Customer.objects \
-                        .annotate(orders_customer=Count('orders')) \
-                        .all() \
+    queryset = OrderItem.objects.annotate(
+        total_price=ExpressionWrapper(F('unit_price') * 0.5, out)
+    )
+    
 
 
 
