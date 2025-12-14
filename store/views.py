19d9abcbd2c413db980128f7c91793bd2e79d28e
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-14 09:28:49 +0330

    14.12.25 09:28:49 Django project changes:
    Modified files:
    - it_auto_log.txt
    - store/templates/hello.html
    - store/views.py

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F, Value, Func
 from django.db.models import Count, Avg, Max
-from django.core.paginator import Paginator
+
 
 
 from .models import Product, OrderItem, Order, Comment, Customer
@@ -12,10 +12,12 @@ def show_data(request):
     queryset = Customer.objects \
                         .annotate(orders_customer=Count('orders')) \
                         .all() \
-    paginator = Paginator(customers, 50)  # 50 رکورد در هر صفحه
-    page = paginator.get_page(1)
+
+
+
     print(queryset)
-    return render(request, 'hello.html')
+
+    return render(request, 'hello.html', {"query": list(queryset)})
 
 
 
