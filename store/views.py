bb8ed35724326e7d52ae4159690fa549190280f8
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-15 11:28:34 +0330

    15.12.25 11:28:33 Django project changes:
    Modified files:
    - tore/views.py

--- store/views.py
+++ store/views.py
@@ -12,11 +12,18 @@ from .models import Product, OrderItem, Order, Comment, Customer
 def show_data(request):    
     # product_1 = Product.objects.get(id=1)
 
-    Comment.objects.create(
-        product_id=1,
-        name='SINA LALEHBAKHSH',
-        body='Sina is DjangoDeveloper and DevOps Engineer',
-    )
+    # Comment.objects.create(
+    #     product_id=1,
+    #     name='SINA LALEHBAKHSH',
+    #     body='Sina is DjangoDeveloper and DevOps Engineer',
+    # )
+
+    new_comment = Comment()
+    new_comment.product = Product.objects.get(id=2)
+    new_comment.name = 'BAHAREH'
+    new_comment.body = 'MY LOVELY WIFE FOR EVER'
+    new_comment.save()
+
 
     return render(request, 'hello.html')
 
