6187b87fdc725da1bd3f5b19795e5484dc5bd5fd
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-28 05:37:14 +0330

    Version 1.1.82 - 28.12.25 05:37:13 Django project changes:
    Modified files:
    - git_auto_version
    - store/admin.py
    - store/models.py

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Product
 from .models import Category
+from .models import Order
 
 
 @admin.register(Product)
@@ -10,6 +11,11 @@ class Product_Admin(admin.ModelAdmin):
     list_editable = ['name', 'inventory', 'unit_price']
     list_per_page = 20
 # admin.site.register(Product, Product_Admin)
+
+
+@admin.register(Order)
+class Order_Admin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'status']
 
 
 
--- store/models.py
+++ store/models.py
@@ -51,6 +51,9 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.first_name}-{self.last_name}'
+
 class Address(models.Model):
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
     province = models.CharField(max_length=255)
