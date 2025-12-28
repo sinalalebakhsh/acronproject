8458992d316c1cc8fdbcde4a29e1face8e9c46d5
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-28 06:32:49 +0330

    Version 1.1.93 - 28.12.25 06:32:48 Django project changes:
    Modified files:
    - git_auto_version
    - store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -11,6 +11,10 @@ class Product_Admin(admin.ModelAdmin):
     list_editable = ['name', 'inventory', 'unit_price']
     list_per_page = 20
     ordering = ['-datetime_created']
+
+    def inventory_status(self, product_ojb):
+
+
 # admin.site.register(Product, Product_Admin)
 
 
