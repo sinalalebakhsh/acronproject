12b5b21de2ce8383ea17925151aca6f97422d391
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-28 05:32:09 +0330

    Version 1.1.81 - 28.12.25 05:32:09 Django project changes:
    Modified files:
    - git_auto_version
    - store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -8,6 +8,7 @@ from .models import Category
 class Product_Admin(admin.ModelAdmin):
     list_display = ['id', 'name', 'inventory', 'unit_price']
     list_editable = ['name', 'inventory', 'unit_price']
+    list_per_page = 20
 # admin.site.register(Product, Product_Admin)
 
 
