e6bcdbaae1ed07fa7e7c6309f53c5e90f1519278
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-28 06:37:54 +0330

    Version 1.1.94 - 28.12.25 06:37:54 Django project changes:
    Modified files:
    - git_auto_version
    - store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -7,12 +7,18 @@ from .models import Order
 
 @admin.register(Product)
 class Product_Admin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'inventory', 'unit_price', 'datetime_created']
+    list_display = ['id', 'name', 'inventory', 'inventory_status', 'unit_price', 'datetime_created']
     list_editable = ['name', 'inventory', 'unit_price']
     list_per_page = 20
     ordering = ['-datetime_created']
 
-    def inventory_status(self, product_ojb):
+    def inventory_status(self, product_object):
+        if product_object.inventory < 10:
+            return 'less than 10'
+        if product_object.inventory == 10:
+            return 'equal with 10'
+        else:
+            return 'greater than 10'
 
 
 # admin.site.register(Product, Product_Admin)
