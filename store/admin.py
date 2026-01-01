001143477bee2b64458eb1ad18d80a2d89f99a46
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-01 10:54:27 +0330

    Version 1.8.70 - 01.01.26 10:54:27 Django project changes:
    Modified files:
    - tore/admin.py

--- store/admin.py
+++ store/admin.py
@@ -103,4 +103,12 @@ class Comment_Admin(admin.ModelAdmin):
 
 
 @admin.register(Customer)
-    
+class Customer_Admin(admin.ModelAdmin):
+    list_display = ['id','first_name','last_name','email','phone_number','birth_date']
+    list_editable = ['first_name','last_name','email','phone_number','birth_date']
+    list_per_page = 20
+    ordering = ['id']
+
+
+
+
