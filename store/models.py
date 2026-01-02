8d0d6464873e0102f249f151a75344635cac8afe
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-02 11:48:28 +0330

    Version 1.9.8 - 02.01.26 11:48:28 Django project changes:
    Added files:
    - store/migrations/0003_alter_product_inventory.py
    
    Modified files:
    - tore/models.py

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
@@ -29,7 +29,7 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6,decimal_places=2)
-    inventory = models.IntegerField(default=0)
+    inventory = models.PositiveIntegerField(default=0)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     discounts = models.ManyToManyField(Discount, blank=True, related_name='products')
