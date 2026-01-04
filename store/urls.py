e7f5e0756c37022d8e3a3006de90e748135f3b65
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-04 20:31:58 +0330

    Version 1.9.20 - 04.01.26 20:31:57 Django project changes:
    Modified files:
    - tore/urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+
+from rest_framework.decorators import api_view
+
+
 from . import views
 
+
+
+@api_view
 urlpatterns = [
     path('products/', views.products_list),
 ]
+
+
+
