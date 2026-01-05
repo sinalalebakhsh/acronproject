35411fb9884ccae8ba26fc6898665356c7874bcd
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-05 20:44:14 +0330

    Version 1.9.25 - 05.01.26 20:44:14 Django project changes:
    Modified files:
    - tore/serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,6 @@
 from rest_framework import serializers
 
-class Product_Serializer(serializers.Serializer)
+class Product_Serializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    unit_price = serializers.DecimalField()
