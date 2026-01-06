4081c26328758a95ef03d47f018cfe5b83a7bf64
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-06 06:35:15 +0330

    Version 1.9.27 - 06.01.26 06:35:15 Django project changes:
    Modified files:
    - tore/views.py

--- store/views.py
+++ store/views.py
@@ -2,8 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-
-
+from store import models 
+from store import serializers 
 
 @api_view()
 def product_list(request):
@@ -13,7 +13,11 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = models.Product.objects.get(pk=id)
+
+    serializer = serializers.Product_Serializer(product)
+
+    return Response(serializer.data)
 
 
 
