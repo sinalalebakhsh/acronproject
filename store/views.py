fdad95e9931dae254553ed27b13b6da37822f95f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-07 22:06:33 +0330

    Version 1.9.30 - 07.01.26 22:06:33 Django project changes:
    Modified files:
    - tore/views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404
+
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -14,10 +17,13 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    try:
-        product = models.Product.objects.get(pk=id)
-    except models.Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    # try:
+    #     product = models.Product.objects.get(pk=id)
+    # except models.Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+    # این خط دقیقا کار چهار خط بالا رو انجام میده
+    product = get_object_or_404(models.Product, pk=id)
+
     serializer = serializers.Product_Serializer(product)
 
     return Response(serializer.data)
