eff035b9382c67a0e53b099e924010d84a87fcee
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-08 23:53:19 +0330

    ADD

--- store/urls.py
+++ store/urls.py
@@ -4,11 +4,11 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "store"
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int: id>/', views.product_detail),
+    path('products/', views.product_list, name="products"),
+    path('products/<int:id>/', views.product_detail, name="products_int_id"),
 
 ]
 
