ef69a25e5fcaa089dd53bf01b6d97e98a14be68b
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-09 23:03:17 +0330

    Version 1.9.45 - 09.01.26 23:03:17 Django project changes:
    Modified files:
    - tore/serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -13,9 +13,9 @@ class ProductSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     # price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    inventory = serializers.IntegerField()
-    price_tomans = serializers.SerializerMethodField()
     with_task = serializers.SerializerMethodField()
+    price_tomans = serializers.SerializerMethodField()
+    inventory = serializers.IntegerField()
 
     def get_price_tomans(self, product):
         return int(product.unit_price * DOLLORS_TO_TOMANS)
