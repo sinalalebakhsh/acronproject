f1ce3bb33e2d278848cfbf5e21ff83d97a4d4f32
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-09 21:47:26 +0330

    Version 1.9.37 - 09.01.26 21:47:25 Django project changes:
    Modified files:
    - tore/serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255, source="name")
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
+    # title = serializers.CharField(max_length=255, source="name")
+    name = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
+    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     inventory = serializers.IntegerField()
 
 
