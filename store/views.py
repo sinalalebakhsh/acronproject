4eb6d474515bced62858e45c1ba87d36f710eb1e
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-01-16 05:55:20 +0330

    Version 1.10.31 - 16.01.26 05:55:20 Django project changes:
    Modified files:
    - tore/views.py

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework.viewsets import ModelViewSet
 from store import models 
 from store import serializers 
 
