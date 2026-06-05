5e2cd43ab112f028dae5f3fb83169db8cf37bde1
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-05 21:21:50 +0330

    store-models.py

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+from uuid import uuid4
+
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
@@ -119,7 +121,10 @@ class Comment(models.Model):
     objects = CommentManger()
     approved = ApprovedCommentManager()
 
+
+
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
